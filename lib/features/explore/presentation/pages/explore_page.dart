63607ac2f94805a583f65f63b08b7ff215c9da9b
pause72 <admin.pause72@gmail.com>
2026-04-16 11:28:09 +0900

    광고 원복

--- lib/features/explore/presentation/pages/explore_page.dart
+++ lib/features/explore/presentation/pages/explore_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
+import '../../../../core/ads/ad_config.dart';
 import '../../../../core/constants/app_strings.dart';
 import '../../../../core/theme/app_colors.dart';
 import '../../../../features/auth/presentation/providers/auth_provider.dart';
@@ -51,6 +53,7 @@ class ExploreContent extends StatelessWidget {
 
 enum _PostFilter { all, review, tip }
 enum _PostSort { recent, popular }
+enum _AdSlot { native }
 
 class ExplorePage extends ConsumerStatefulWidget {
   const ExplorePage({super.key});
@@ -62,6 +65,44 @@ class ExplorePage extends ConsumerStatefulWidget {
 class _ExplorePageState extends ConsumerState<ExplorePage> {
   _PostFilter _filter = _PostFilter.all;
   _PostSort _sort = _PostSort.recent;
+  NativeAd? _nativeAd;
+  bool _isNativeAdLoaded = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadNativeAd();
+  }
+
+  void _loadNativeAd() {
+    _nativeAd = NativeAd(
+      adUnitId: AdConfig.nativeAdUnitId,
+      request: const AdRequest(),
+      nativeTemplateStyle: NativeTemplateStyle(templateType: TemplateType.medium),
+      listener: NativeAdListener(
+        onAdLoaded: (_) => setState(() => _isNativeAdLoaded = true),
+        onAdFailedToLoad: (ad, _) => ad.dispose(),
+      ),
+    )..load();
+  }
+
+  @override
+  void dispose() {
+    _nativeAd?.dispose();
+    super.dispose();
+  }
+
+  // 5개 게시글마다 네이티브 광고 삽입
+  List<Object> _buildMixedList(List<CommunityPost> posts) {
+    final List<Object> mixed = [];
+    for (int i = 0; i < posts.length; i++) {
+      mixed.add(posts[i]);
+      if ((i + 1) % 5 == 0 && _isNativeAdLoaded) {
+        mixed.add(_AdSlot.native);
+      }
+    }
+    return mixed;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -117,9 +158,10 @@ class _ExplorePageState extends ConsumerState<ExplorePage> {
                     );
                   }
 
+                  final mixedItems = _buildMixedList(filtered);
                   return ListView.builder(
                     padding: const EdgeInsets.fromLTRB(16, 4, 16, 80),
-                    itemCount: filtered.length + 1, // +1 for motivation banner
+                    itemCount: mixedItems.length + 1, // +1 for motivation banner
                     itemBuilder: (_, i) {
                       // 첫 번째 아이템: 동기부여 배너
                       if (i == 0) {
@@ -128,7 +170,14 @@ class _ExplorePageState extends ConsumerState<ExplorePage> {
                           child: _MotivationBanner(posts: posts),
                         );
                       }
-                      final post = filtered[i - 1];
+                      final item = mixedItems[i - 1];
+                      if (item == _AdSlot.native) {
+                        return Padding(
+                          padding: const EdgeInsets.only(bottom: 12),
+                          child: SizedBox(height: 320, child: AdWidget(ad: _nativeAd!)),
+                        );
+                      }
+                      final post = item as CommunityPost;
                       return Padding(
                         padding: const EdgeInsets.only(bottom: 12),
                         child: _PostCard(
